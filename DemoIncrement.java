ce422ed767cadc80aac36767355a7ae04f663950
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-02-06 10:05:56 +0000

    DemoIncrement completed

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -1,3 +1,22 @@
-// Add your code below
+public class DemoIncrement {
+    public static void main(String[] args) {
+        // Demonstration of the increment operator
+        int a = 3;
+        int b = a++ + 5;
+        int x = 3;
+        int y = ++x + 5;
+        int p = 3;
+        // ++p and p++ give the same result because the increment is not in an expression.
+        ++p;
+        int q = p + 5; 
+
+        System.out.println("a: " + a + ", b: " + b + ", x: " + x + ", y: " + y + ", p: " + p + ", q: " + q);
+        System.out.println("i++ means post-increment");
+        System.out.println("++i means pre-increment");
+
+    }
+}
+
+
 
 
