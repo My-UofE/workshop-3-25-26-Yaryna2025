3b829b6641d0c60c5014e7e606b7b517cd581944
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-02-06 10:28:38 +0000

    NumbersInSpanish completed with if - else if - else

---
+++ NumbersInSpanish.java
@@ -0,0 +1,19 @@
+public class NumbersInSpanish {
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+
+        if (number == 1) {
+            System.out.println("uno");
+        } else if (number == 2) {
+            System.out.println("dos");
+        } else if (number == 3) {
+            System.out.println("tres");
+        } else if (number == 4) {
+            System.out.println("cuatro");
+        } else if (number == 5) {
+            System.out.println("cinco");
+        } else {
+            System.out.println("Sorry I do not know that!");
+        }
+    }
+}
